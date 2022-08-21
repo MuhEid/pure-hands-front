dd1850db748ad31300254c1122cd8218f08de81b
MuhEid96 <moh.eid.1996@gmail.com>
2022-08-21 22:59:43 +0200

    feature: create home and footer structure

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Button from '../UI/Button';
+import Typography from '../UI/Typography';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button variant={'primary'} className="text-white">
+        tesst
+      </Button>
+      <Typography component="h5">Home</Typography>
+    </div>
+  );
 }
