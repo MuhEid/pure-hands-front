3ad9b484450297af69c0f9b8f9d76763975b9bf0
MuhEid96 <moh.eid.1996@gmail.com>
2022-08-28 20:35:57 +0200

    feature: add backgroung color and container class

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'primary-50': '#75846e',
         'primary-100': '#526E3E',
         'primary-200': '#44533c',
+        'background-100': '#F1F0EC',
+        'background-200': '#EBE9E2',
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
       },
     },
   },
