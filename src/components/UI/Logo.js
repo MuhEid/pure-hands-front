5ac4826bceee67accdf280dceaf4be2f5c90eac1
MuhEid96 <moh.eid.1996@gmail.com>
2022-08-28 20:35:15 +0200

    refactor: change letter spacing

--- src/components/UI/Logo.js
+++ src/components/UI/Logo.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 export default function Logo() {
   return (
     <Link to="/">
-      <p className="font-[wintersoul] text-4xl p-1">
+      <p className="font-[wintersoul] text-center lg:text-left tracking-wide text-4xl">
         Pure
-        <span className="font-[valkyrie] text-primary-100">Hands</span>
+        <span className="ml-1 font-[valkyrie] text-primary-100">Hands</span>
       </p>
     </Link>
   );
